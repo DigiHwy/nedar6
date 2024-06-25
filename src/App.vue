b467d4947c3a475254a1b5498a86c2c6ad4ce081
Carme <carme@digihwy.com>
2024-06-25 15:00:14 +0800

    add css, change title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
 import TheMap from './components/TheMap.vue';
+import { IconifyIcon } from '@iconify/vue';
 </script>
 
 <template>
-  <TheMap />
+  <div>
+    <HeaderIcons />
+    <div class="parent-container" id="parent-container">
+    </div>
+    <div class="map-container" id="map-container">
+      <TheMap />
+    </div>
+        <div class="text-container" id="text-container">
+      <h1>WESTERN VISAYAS</h1>
+      <h3>INVESTMENT GUIDE</h3>
+    </div>
+  </div>
 </template>
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
-
 import OpenLayersMap from 'vue3-openlayers';
 import 'vue3-openlayers/dist/vue3-openlayers.css';
-
 import App from './App.vue';
+import { Icon } from '@iconify/vue';
+import mdiLandmark from '@iconify-icons/mdi-light/landmark';
+import mdiBuilding from '@iconify-icons/mdi-light/building';
+// Import other icons as needed
 
 createApp(App).use(OpenLayersMap).mount('#app');
